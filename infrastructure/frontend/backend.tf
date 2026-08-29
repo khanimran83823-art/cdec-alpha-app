d4a736425925c319b3333c2e6870b5ba8ca7f8fa
khanimran83823 <khanimran83823@gmail.com>
2026-08-29 19:57:56 +0530

    Change S3 backend region to ap-south-1

--- infrastructure/frontend/backend.tf
+++ infrastructure/frontend/backend.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket  = "cdec-alpha-terraform-state-atulyw"
     key     = "frontend/terraform.tfstate"
-    region  = "eu-west-1"
+    region  = "ap-south-1"
     encrypt = true
     # profile = "terraform-sessions"
 
